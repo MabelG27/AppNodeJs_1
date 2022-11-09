8d005de75b3fc81f76e138b2f2453de028d4f94a
MabelG27 <mabelgarcia_d27@hotmail.com>
2022-11-09 00:29:15 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const webstomp = require('webstomp-client');
+const WebSocket =  require(' ws');
+
+const app = express();
+const port = 3002;
+const client = webstomp.over(new WebSocket('ws://127.0.0.1:15674/ws'));
+
+function onError(user, err){
+    console.log('Desconenctado', user, name, error);
+
+}
+
+let message = "Hola Mundo!!!";
+function onConnect(user){
+    console.log('Conectado', user, name);
+    client.send('');
+}
+
+
+
+app.listen(port, () => console.log(`App listening on port ${port}`));

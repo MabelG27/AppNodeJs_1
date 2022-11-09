c64031f9880c4393a8546eb13b04984b4c2d02dd
MabelG27 <mabelgarcia_d27@hotmail.com>
2022-11-09 19:35:49 -0400

    Editando app..js

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const WebSocket =  require(' ws');
 
 const app = express();
 const port = 3002;
-const client = webstomp.over(new WebSocket('ws://127.0.0.1:15674/ws'));
+const client = webstomp.over(new WebSocket('http://127.0.0.1:15674/ws'));
 
 function onError(user, err){
-    console.log('Desconenctado', user, name, error);
+    console.log('Desconectado', user, name, error);
 
 }
 
